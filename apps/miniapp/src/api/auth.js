a9fcc8a9082e4e92f29c30e1b2d0a217d95ec3c0
Chin-xiao <chinnzztheking09@gmail.com>
2026-06-04 10:42:01 +0700

    Anh jg push mixx

--- apps/miniapp/src/api/auth.js
+++ apps/miniapp/src/api/auth.js
@@ -8,7 +8,6 @@ export const loginDev = async (telegramId, name) => {
     })
     return res.data
 }
-
 export const loginTelegram = async (initData) => {
     const res = await client.post('/auth/telegram', { init_data: initData })
     return res.data
